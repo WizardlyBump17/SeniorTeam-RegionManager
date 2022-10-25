10a96c9858091570b87f36aee11d6722162eb98a
WizardlyBump17 <davicraft130@gmail.com>
2022-10-25 08:51:22 -0300

    the RegionFlag can be loaded from a ResultSet now

--- api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/RegionFlag.java
+++ api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/RegionFlag.java
@@ -1,9 +1,15 @@
 package com.wizardlybump17.seniorteam.regionmanager.api.region.flag;
 
+import com.wizardlybump17.seniorteam.regionmanager.api.cache.RegionFlagTypeCache;
 import com.wizardlybump17.seniorteam.regionmanager.api.region.flag.type.RegionFlagType;
 import com.wizardlybump17.seniorteam.regionmanager.api.region.flag.value.RegionFlagValue;
+import com.wizardlybump17.seniorteam.regionmanager.api.util.BukkitStreamsUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +18,16 @@ public class RegionFlag {
     private final String name;
     private final RegionFlagType<?> type;
     private RegionFlagValue<?> value;
+
+    public static RegionFlag load(ResultSet set, RegionFlagTypeCache typeCache) throws SQLException {
+        Optional<RegionFlagType<?>> typeOptional = typeCache.get(set.getString("type"));
+        if (typeOptional.isEmpty())
+            return null;
+
+        return new RegionFlag(
+                set.getString("name"),
+                typeOptional.get(),
+                (RegionFlagValue<?>) BukkitStreamsUtil.deserialize(set.getBytes("value"))
+        );
+    }
 }
--- api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/value/RegionFlagValue.java
+++ api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/value/RegionFlagValue.java
@@ -1,8 +1,19 @@
 package com.wizardlybump17.seniorteam.regionmanager.api.region.flag.value;
 
-public interface RegionFlagValue<T> {
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public interface RegionFlagValue<T> extends ConfigurationSerializable {
 
     Class<T> getType();
 
     T getValue();
+
+    @NotNull
+    @Override
+    default Map<String, Object> serialize() {
+        return Map.of("value", getValue());
+    }
 }

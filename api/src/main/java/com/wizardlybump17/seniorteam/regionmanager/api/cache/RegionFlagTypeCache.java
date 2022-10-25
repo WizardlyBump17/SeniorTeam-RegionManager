4aa0a3d392b085c34fcc50fb40d99ea8719e566f
WizardlyBump17 <davicraft130@gmail.com>
2022-10-25 08:50:19 -0300

    added RegionFlagTypeCache

---
+++ api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/cache/RegionFlagTypeCache.java
@@ -0,0 +1,32 @@
+package com.wizardlybump17.seniorteam.regionmanager.api.cache;
+
+import com.wizardlybump17.seniorteam.regionmanager.api.region.flag.type.RegionFlagType;
+import com.wizardlybump17.wlib.object.Cache;
+import com.wizardlybump17.wlib.object.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class RegionFlagTypeCache extends Cache<String, RegionFlagType<?>, RegionFlagType<?>> {
+
+    @Override
+    public @NotNull Pair<String, RegionFlagType<?>> apply(RegionFlagType<?> type) {
+        return new Pair<>(type.getName().toLowerCase(), type);
+    }
+
+    @Override
+    public @NotNull Optional<RegionFlagType<?>> get(@Nullable String key) {
+        return super.get(key == null ? null : key.toLowerCase());
+    }
+
+    @Override
+    public boolean has(@Nullable String key) {
+        return super.has(key == null ? null : key.toLowerCase());
+    }
+
+    @Override
+    public Optional<RegionFlagType<?>> remove(@Nullable String key) {
+        return super.remove(key == null ? null : key.toLowerCase());
+    }
+}

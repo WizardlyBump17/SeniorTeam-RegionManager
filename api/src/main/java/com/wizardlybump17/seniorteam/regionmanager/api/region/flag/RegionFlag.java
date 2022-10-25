a26e2a4c3157ad85ee6b69ed5e6b7690e2ca59e8
WizardlyBump17 <davicraft130@gmail.com>
2022-10-25 10:46:13 -0300

    make RegionFlag implements DatabaseStorable

--- api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/RegionFlag.java
+++ api/src/main/java/com/wizardlybump17/seniorteam/regionmanager/api/region/flag/RegionFlag.java
@@ -4,20 +4,52 @@ import com.wizardlybump17.seniorteam.regionmanager.api.cache.RegionFlagTypeCache
 import com.wizardlybump17.seniorteam.regionmanager.api.region.flag.type.RegionFlagType;
 import com.wizardlybump17.seniorteam.regionmanager.api.region.flag.value.RegionFlagValue;
 import com.wizardlybump17.seniorteam.regionmanager.api.util.BukkitStreamsUtil;
-import lombok.AllArgsConstructor;
+import com.wizardlybump17.wlib.database.DatabaseStorable;
 import lombok.Data;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Optional;
 
 @Data
-@AllArgsConstructor
-public class RegionFlag {
+public class RegionFlag implements DatabaseStorable {
 
     private final String name;
     private final RegionFlagType<?> type;
     private RegionFlagValue<?> value;
+    private boolean dirty;
+    private boolean deleted;
+    private boolean inDatabase;
+
+    public RegionFlag(String name, RegionFlagType<?> type, RegionFlagValue<?> value) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public void setValue(RegionFlagValue<?> value) {
+        this.value = value;
+        dirty = true;
+    }
+
+    @Override
+    public void saveToDatabase(Map<String, Object> data) {
+        data.put("name", name);
+        data.put("type", type.getName());
+        data.put("value", BukkitStreamsUtil.serialize(value));
+    }
+
+    @Override
+    public void updateToDatabase(Map<String, Object> where, Map<String, Object> data) {
+        where.put("name", name);
+        data.put("value", BukkitStreamsUtil.serialize(value));
+    }
+
+    @Override
+    public void deleteFromDatabase(Map<String, Object> data) {
+        data.put("name", name);
+    }
 
     public static RegionFlag load(ResultSet set, RegionFlagTypeCache typeCache) throws SQLException {
         Optional<RegionFlagType<?>> typeOptional = typeCache.get(set.getString("type"));
